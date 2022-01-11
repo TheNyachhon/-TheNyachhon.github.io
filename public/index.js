5b8b68ac4ede2837ae20db19ae160c28e04e842c
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2022-01-11 22:35:07 +0530

    projects tab updated 💻✅

--- public/index.js
+++ public/index.js
@@ -121,25 +121,3 @@ function darkToLight() {
         document.querySelector('nav ul').style.fontWeight = '800'
     }, 1000)
 }
-
-// In progress
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-  });
